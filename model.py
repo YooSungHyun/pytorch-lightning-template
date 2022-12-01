a5c4e939883fa0a03d1d4033b6c7f032698ed309
YooSungHyun <shyu0522@hanmail.net>
2022-12-01 18:38:40 +0900

    Update: valid on cpu way description

--- model.py
+++ model.py
@@ -9,7 +9,9 @@ class CustomNet(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters(args)
         self.args = args
-        self.loss_func = MeanSquaredError()
+        # TODO: If you want to learn eval metric on cpu. just use compute_on_cpu=True
+        # and follow validation_step
+        self.loss_func = MeanSquaredError(compute_on_cpu=False)
         # TODO: Write down your network
         self.dense_batch_fc_tanh = nn.Sequential(
             nn.Linear(args.input_dense_dim, args.output_dense_dim),
@@ -34,6 +36,22 @@ class CustomNet(pl.LightningModule):
         return {"loss": loss}
 
     def validation_step(self, batch, batch_idx):
+        """validation_step
+        if you want to run validation_step on cpu, follow this,
+        1. All variable move to cpu
+            features.cpu()
+            labels.cpu()
+            feature_lengths.cpu()
+            label_lengths.cpu()
+        2. model move to cpu
+            self.cpu()
+        3. eval step going
+
+        Args:
+            batch (_type_): validation datasets batch
+            batch_idx (_type_): batch's index
+
+        """
         features, labels, feature_lengths, label_lengths = batch
         logits = self(features)
         loss = self.loss_func(logits, labels)
@@ -41,6 +59,18 @@ class CustomNet(pl.LightningModule):
         return {"loss": loss}
 
     def validation_epoch_end(self, validation_step_outputs):
+        """validation_epoch_end
+        if you want to run validation_step on cpu, follow this,
+        1. your validation_step_outputs is already on cpu
+        2. if ddp, each machine output must gather. and lightning can gather only on-gpu items
+            self.log("val_loss", loss_mean.cuda(), sync_dist=True)
+            self.cuda() -> model have to training_step on cuda
+
+        Args:
+            batch (_type_): validation datasets batch
+            batch_idx (_type_): batch's index
+
+        """
         loss_mean = torch.tensor([x["loss"] for x in validation_step_outputs], device=self.device).mean()
         self.log("val_loss", loss_mean, sync_dist=(self.device != "cpu"))
         # self.log_dict(metrics, sync_dist=(self.device != "cpu"))
